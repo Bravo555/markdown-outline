f63f55167f7b8e4109979311a24da2a8916a4dad
Marcel Guzik <mguzik555@gmail.com>
2017-01-05 13:09:35 +0100

    I'm a dim person

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='markdown_outline',
+    version='0.1',
+    description='Creates outline of a Markdown document',
+    url='https://github.com/Bravo555/markdown-outline',
+    author='Bravo555',
+    author_email='mguzik555@gmail.com',
+    license='MIT',
+    packages=['markdown_outline'],
+    zip_safe=False)
