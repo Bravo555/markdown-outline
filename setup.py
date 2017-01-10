e2988fe69dbfdeda13cd4d239314006982913d66
Marcel Guzik <mguzik555@gmail.com>
2017-01-10 14:14:59 +0100

    Made executable script and restructured package files a bit

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 setup(name='markdown_outline',
-    version='0.1',
+    version='0.0.1',
     description='Creates outline of a Markdown document',
     url='https://github.com/Bravo555/markdown-outline',
     author='Bravo555',
     author_email='mguzik555@gmail.com',
     license='MIT',
     packages=['markdown_outline'],
+    scripts=['bin/markdown-outline'],
     zip_safe=False)
